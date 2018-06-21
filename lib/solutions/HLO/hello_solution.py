8cd7d70304a90522c0a51a0df4e3dac8f8f3381a
sbx_user1059 <sbx_user1059@ip-10-12-139-78.eu-west-2.compute.internal>
2018-06-21 06:46:13 +0000

    Thu Jun 21 06:46:13 UTC 2018

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,7 +3,4 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    if friend_name == "x":
-        return "Hello, World!"
-    else:
-        return "Hello, "+friend_name+"!"
+    raise NotImplementedError()
